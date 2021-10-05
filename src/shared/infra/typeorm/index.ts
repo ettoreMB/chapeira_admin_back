f757bee4e2c29ee41056707cef9f84c8b42a5896
Ettore <ettore.barbosa@gmail.com>
2021-10-05 20:00:10 -0300

    testFile

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,13 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "chapeira.com.br"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "chapeira.com.br" : host,
+
       database:
         process.env.NODE_ENV === "test"
-          ? "rentx_test"
+          ? "chapeira_test"
           : defaultOptions.database,
     })
   );
