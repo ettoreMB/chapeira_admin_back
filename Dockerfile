443e71f1df7226608a37ec25f9248676a154dd01
Ettore <ettore.barbosa@gmail.com>
2022-02-10 19:32:08 -0300

    docker fix

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,12 @@ COPY package.json ./
 
 RUN npm install
 
+RUN npm install pm2 -g
+
 COPY . .
 
-CMD ["npm", "run", "dev"]
+RUN npm run build
+
+CMD ["pm2-runtime", "dist/shared/infra/http/server.js"]
 
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,6 @@ import cors from "cors";
 import { app } from "./App";
 
 app.use(cors());
-app.listen(3333, () => {
+app.listen(3333, '0.0.0.0', () => {
   console.log("server is Running on Port 3333");
 });
